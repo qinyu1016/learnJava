d6f5836a678ad779e13177cbc07d3e0cbabfe0fc
Your Name <you@example.com>
2022-07-27 15:54:24 +0800

    添加了排除.idea

---
+++ untitled/src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    public void goTONightClass(){
+        System.out.println("我们在认真上晚自习");
+    }
+
+    public void bubbleLibrary(){
+        System.out.println("我们在泡图书馆");
+    }
+
+    public void attendClass(){
+        System.out.println("我们在认真上课");
+    }
+
+    public void doRadioGymnastics(){
+        System.out.println("我们在认真做广播体操");
+    }
+}
